d51a1c09bba77ab66a1ca21ad7af00f99aba869a
Debarshi Chatterjee <debarshi.it.aec@gmail.com>
2025-03-10 00:02:47 +0530

    Add files via upload

--- Functions/function1.java
+++ Functions/function1.java
@@ -1,1 +1,13 @@
+class ob{
+    public static void name (String name){
+        System.out.println(name);
+        return;
+    }
+}
 
+public class function1{
+    public static void main (String args[]){
+        ob obj=new ob();
+        obj.name("Debarshi");
+    }
+} 
